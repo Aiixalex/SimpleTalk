dd3c3b3a57a6afbb9e5f89b5d92e25852a46ac59
Aiixalex <redcloveralex@gmail.com>
2021-11-02 22:43:33 -0700

    fix: fix a bug about buf_size checking

--- udp_client.c
+++ udp_client.c
@@ -72,10 +72,9 @@ void* UdpSendto(void* message_queue) {
         }
 
         int buf_size = sendto(sfd, message, strlen(message), 0, rp->ai_addr, rp->ai_addrlen);
-        printf("%d %d\n", buf_size, strlen(message) + 1);
         if (buf_size == -1) {
             handle_error("sendto() failed");
-        } else if (buf_size != strlen(message) + 1) {
+        } else if (buf_size != strlen(message)) {
             fprintf(stderr, "sendto() partial failed.\n");
             exit(EXIT_FAILURE);
         }
